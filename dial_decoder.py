7adb9477ea49648fff819f59f57d3c54addbd99d
Salam Waddah <salamwaddah@gmail.com>
2018-02-11 15:51:25 +0400

    Feature: consider multiple results

--- dial_decoder.py
+++ dial_decoder.py
@@ -6,9 +6,19 @@ user_input = "+" + str(user_input)
 
 print
 
+found_results = []
 for country_record in data:
 	for dial_num in country_record:
 		if user_input == country_record[dial_num]:
-			print "Name: " + str(country_record["name"])
-			print "Code: " + str(country_record["code"])
-			print "Dial Number: " + str(country_record["dial_code"])
+			found_results.append(country_record)
+
+if not found_results:
+	print "No matches."
+else:
+	print len(found_results), "result(s) found."
+	print
+	for result in found_results:
+		print "Name: " + result["name"]
+		print "Code: " + result["code"]
+		print "Dial Number: " + result["dial_code"]
+		print
