f334f9787dcd58b702d19132607a88e597b04ebf
Salam Waddah <salamwaddah@gmail.com>
2018-02-11 19:44:53 +0400

    Fix #1 user is only allowed to enter numbers now

--- dial_decoder.py
+++ dial_decoder.py
@@ -1,10 +1,15 @@
 import json
 from beautifultable import BeautifulTable
 
+def is_number(s):
+	while s.isdigit() == False:
+		s = raw_input("Enter only numbers: ")
+	return int(s)
+
 data = json.load(open('data.json'))
 table = BeautifulTable()
 
-user_input = input("Enter country code: ")
+user_input = is_number(raw_input("Enter country code: "))
 user_input = "+" + str(user_input)
 
 print
